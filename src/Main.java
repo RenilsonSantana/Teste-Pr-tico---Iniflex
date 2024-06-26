b9b8bdcaca814c210663d1315b165eb56d907116
Renilson Santos <renilson_bmn@hotmail.com>
2024-06-26 16:27:04 -0300

    remove comments in Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,24 +25,5 @@ public class Main {
 		funcionarios.exibirFuncionariosOrdemAlfabetica();
 		funcionarios.exibirTotalSomaSalarios();
 		funcionarios.exibirQuantidadeDeSalariosCadaFuncionarioGanha();
-		
-		
-		
-//		List<Funcionario> lfuncionarios = new ArrayList<Funcionario>();
-//		
-//		
-//		
-//		lfuncionarios.add(new Funcionario("Maria", LocalDate.of(2000, 10, 18), BigDecimal.valueOf(2009.44), "Operador"));
-//		lfuncionarios.add(new Funcionario("João", LocalDate.of(1990, 05, 12), BigDecimal.valueOf(2284.38), "Operador"));
-//		lfuncionarios.add(new Funcionario("Caio", LocalDate.of(1961, 05, 02), BigDecimal.valueOf(9836.14), "Coordenador"));
-//		lfuncionarios.add(new Funcionario("Miguel", LocalDate.of(1988, 10, 14), BigDecimal.valueOf(19119.88), "Diretor"));
-//		lfuncionarios.add(new Funcionario("Alice", LocalDate.of(1995, 01, 05), BigDecimal.valueOf(2234.68), "Recepcionista"));
-//		lfuncionarios.add(new Funcionario("Heitor", LocalDate.of(1999, 11, 19), BigDecimal.valueOf(1582.72), "Operador"));
-//		lfuncionarios.add(new Funcionario("Arthur", LocalDate.of(1993, 03, 31), BigDecimal.valueOf(4071.84), "Contador"));
-//		lfuncionarios.add(new Funcionario("Laura", LocalDate.of(1994, 07, 8), BigDecimal.valueOf(3017.45), "Gerente"));
-//		lfuncionarios.add(new Funcionario("Heloísa", LocalDate.of(2003, 05, 24), BigDecimal.valueOf(1606.85), "Eletricista"));
-//		lfuncionarios.add(new Funcionario("Helena", LocalDate.of(1996, 9, 02), BigDecimal.valueOf(2799.93), "Gerente"));
-		
-		
 	}
 }
